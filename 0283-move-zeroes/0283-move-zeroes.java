babf416d538532a09a6290dd12f6670a40b70590
Y. Sri Keerthi <147797134+srikeerthireddy@users.noreply.github.com>
2026-04-01 10:12:39 +0530

    Time: 2 ms (90.86%), Space: 47.2 MB (98.5%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,11 +3,12 @@ class Solution {
         int index=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                int temp=nums[i];
-                nums[i]=nums[index];
-                nums[index]=temp;
+                int temp=nums[index];
+                nums[index]=nums[i];
+                nums[i]=temp;
                 index++;
             }
+
         }
     }
 }
