d91bfdab9484ae454f319b16a24e1e585b4025ec
Y. Sri Keerthi <147797134+srikeerthireddy@users.noreply.github.com>
2026-05-18 09:20:48 +0530

    Time: 0 ms (100%), Space: 48.2 MB (68.09%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -3,7 +3,7 @@ class Solution {
         int left=0;
         int right=arr.length-1;
         while(left<=right){
-            int mid=left+(right-left)/2;
+            int mid=(left+right)/2;
 
             if(arr[mid]==target){
                 return mid;
